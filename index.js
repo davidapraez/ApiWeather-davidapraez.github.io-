cee6957586b4abaf9c327cd3bb45dadfe1191290
davidapraez <omarapraez@hotmail.com>
2022-09-21 06:34:17 -0500

    Primer commit

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const cleanUp = ()=>{
 
 const getDate = () => {
     let date = new Date();
-    return `${date.getDate()}-${((date.getMonth()+1))}-${date.getFullYear()}`
+    return `${date.getDate()}-${('0'+((date.getMonth()+1)))}-${date.getFullYear()}`
 }
 
 const cargarubicacion = () => {
